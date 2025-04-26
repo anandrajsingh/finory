a60dfec0a492e1b805747d18957eeb6f92b7aa09
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-04-26 11:18:51 +0530

    react router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='flex items-center justify-center'>
-      hello
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+    </Routes>
   )
 }
 
